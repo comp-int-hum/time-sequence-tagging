e2a6c13d1e6ba5539ea554a83bb2172ba03517b0
Sabrina Li <sabrina.xin.li@gmail.com>
2023-07-31 21:46:23 -0400

    Adding code for multi-volume books -- unfinished

--- scripts/gutenberg.py
+++ scripts/gutenberg.py
@@ -29,7 +29,7 @@ def get_metadata(soup):
     data = {}
     title_stmt = soup.find('title').string.split(' by ')
     data["title"] = title_stmt[0][:-1] if title_stmt[0][-1] == "," else title_stmt[0]
-    data["author"] = title_stmt[1].strip()
+    data["author"] = title_stmt[1].strip() if len(title_stmt) > 1 else None
 
     # This is just to keep the format consistent with the data gathered from Women Writers Project
     data["edition"] = None
@@ -37,6 +37,23 @@ def get_metadata(soup):
     data["form"] = None
 
     return data
+
+def get_signifier_words():
+    return ["contents", "content", "volume", "book"]
+
+def is_volume_header(header):
+    signifier_words = get_signifier_words()
+    for word in signifier_words:
+        if word in header:
+            return True
+    return False
+
+def is_content_table(table):
+    signifier_words = get_signifier_words()
+    for word in signifier_words:
+        if word in table.previous_sibling.string.lower():
+            return True
+    return False
 
 def get_chapter_links(soup):
     # ch_links = []
@@ -46,28 +63,34 @@ def get_chapter_links(soup):
     #         ch_links.extend(a.find_all('a'))
         
     # return ch_links
-    ch_links = []
-    toc = soup.find_all(attrs={"class":"toc"})
-    if toc:
-        for instance in toc:
+    volume_links = []
+    paragraph_toc_class = soup.find_all('p', attrs={"class":"toc"})
+    if paragraph_toc_class:
+        ch_links = []
+        for instance in paragraph_toc_class:
             links = instance.find_all('a')
             if links:
                 ch_links.extend(instance.find_all('a'))
-        
+        volume_links[" "] = ch_links # Default behavior for one volume book
     elif soup.find(attrs={"class":"chapter"}):
         tables = soup.find_all('table')
-        signifier_words = ["Contents", "Content"]
-        chapter_table = None
         for table in tables:
-            if table.previous_sibling.string in signifier_words:
-                chapter_table = table
-        if chapter_table:
-            links = chapter_table.find_all('a')
-            if links:
-                ch_links.extend(links)
+            if is_content_table(table):
+                ch_links = chapter_table.find_all('a')
+                if ch_links:
+                    volume_links[table.previous_sibling.string] = ch_links # Use header of table as name of volume
+    else:
+        toc_div = soup.find('div', attrs={"class":"toc"}):
+        if toc_div:
+            headers = toc_div.find_all('h2')
+            ch_links = []
+            if headers:
+                for header in headers:
+                    if is_volume_header(header):
+                        header.find_next_sibling()
     
-    return ch_links
-    
+    return volume_links
+
 def get_chapters(soup, ch_links):
     
     cnum = len(ch_links)
@@ -76,11 +99,11 @@ def get_chapters(soup, ch_links):
         ch_start = ch_links[i].get('href')[1:]
         ch_end = ch_links[i+1].get('href')[1:] if (i+1) < cnum else None
 
-        print(f"Start: {ch_start}")
+        # print(f"Start: {ch_start}")
         # print(f"ch_start href: {ch_start.get('href')}")
         # print(f"ch_end href: {ch_end.get('href')}")
         start = soup.find('a', id=ch_start) or soup.find('a', attrs = {"name":ch_start})
-        print(f"Start mod: {start}")
+        # print(f"Start mod: {start}")
         end = soup.find('a', id=ch_end) or soup.find('a', attrs = {"name": ch_end}) if ch_end else None
         
         paragraph_dict = {}
@@ -112,22 +135,26 @@ if __name__ == "__main__":
     args, rest = parser.parse_known_args()
 
     print(f"Input: {args.input}")
-    result = {}
+    data = []
     with open(args.input) as catalog:
         csv_reader = csv.DictReader(catalog)
         for row in csv_reader:
-            locc = row["LoCC"].split(";")
-            is_lang_lit = any(tag[0] == "P" for tag in locc)
+            locc = row["LoCC"].split(";") if row["LoCC"] else None
+            is_lang_lit = any(tag[0] == "P" for tag in locc) if locc else None
             if is_lang_lit:
                 text_num = row["Text#"]
                 file_path = get_gb_html_dir(args.base_dir, text_num)
                 print(f"Text number: {text_num}")
                 print(f"File Path: {file_path}")
-                with open(file_path, "r") as fpointer:
-                    soup = BeautifulSoup(fpointer, "html.parser")
-                    result = get_metadata(soup)
-                    ch_links = get_chapter_links(soup)
-                    result["segments"] = get_chapters(soup, ch_links)
+                if os.path.isfile(file_path):
+                    with open(file_path, "rb") as fpointer:
+                        soup = BeautifulSoup(fpointer, "html.parser", from_encoding='UTF-8')
+                        print(soup.original_encoding)
+                        result = get_metadata(soup)
+                        volume_links = get_chapter_links(soup)
+                        for volume in volume_links:
+                            result["segments"] = get_chapters(soup, volume)
+                            data.append(result)
     
             
 
@@ -144,7 +171,7 @@ if __name__ == "__main__":
     #     result["segments"] = get_chapters(soup, chapter_links)
 
     with open(args.output, "w") as output:
-        json.dump(result, output)
+        json.dump(data, output)
                 
 
     # html_files = find_html_files(args.base_dir)
