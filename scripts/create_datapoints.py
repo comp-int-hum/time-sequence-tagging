b9718ede3b4ebfc8b3c88c2964e4e72e12c36c30
Sabrina Li <project.xibrin@gmail.com>
2023-08-07 17:03:41 -0400

    Update for train/test

---
+++ scripts/create_datapoints.py
@@ -0,0 +1,87 @@
+import jsonlines
+from collections import OrderedDict
+import argparse
+import random
+# Input: encoded segments grouped by paragraphs
+# Output: list of encoded segments grouped by chapter
+def get_embeddings_for_chapter(encoded_chapter):
+    embeddings = []
+    for i in range(len(encoded_chapter)):
+        p_name = "p" + str(i)
+        embeddings.extend(encoded_chapter[p_name])
+    return embeddings
+
+def split_chapter(encoded_chapter):
+    first_half = []
+    second_half = []
+    half = len(encoded_chapter) / 2
+    for i, sent_embedding in enumerate(encoded_chapter):
+        if i < half:
+            first_half.append(sent_embedding)
+        else:
+            second_half.append(sent_embedding)
+
+    return (first_half, second_half)
+
+def get_first_half(encoded_chapter):
+    return split_chapter(encoded_chapter)[0]
+
+def get_second_half(encoded_chapter):
+    return split_chapter(encoded_chapter)[1]
+
+def average_embeddings(sent_embeddings):
+    return [sum(parameter) / len(sent_embeddings) for parameter in zip(*sent_embeddings)]
+
+# input: chapter num and encoded_text
+
+# input: metadata is a dict
+def create_datapoint(metadata, first, second):
+    datapoint = metadata.copy()
+    datapoint["first"] = average_embeddings(first)
+    datapoint["second"] = average_embeddings(second)
+    return datapoint
+
+# # Input: number of times to sample
+# # Output: samples
+# def sample_chapters(num_samples, encoded_text):
+#     num_chapters = len(encoded_text)
+#     random_samples = random.sample(range(0, num_chapters-1), num_samples)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", dest="input", help="Encoded file")
+    parser.add_argument("--output", dest="output", help="Name for datapoints file")
+    parser.add_argument("--samples", type=int, dest="samples", help="Number of samples to take")
+    args, rest = parser.parse_known_args()
+    
+    with jsonlines.open(args.input, "r") as input, jsonlines.open(args.output, mode="w") as writer:
+        # For line in jsonlines
+        data = []
+        for idx, text in enumerate(input):
+            encoded_data = {}
+            encoded_data["title"] = text["title"]
+            encoded_data["author"] = text["author"]
+            encoded_data["edition"] = text["edition"]
+            encoded_data["pub_info"] = text["pub_info"]
+            
+            chapters = text["encoded_segments"]
+            num_chapters = len(chapters)
+            num_samples = min(num_chapters, args.samples)
+            sample_list = random.sample(range(0, num_chapters-1), num_samples)
+
+            for cnum in sample_list:
+                first_ch = split_chapter(chapters[cnum])
+                positive_dp = create_datapoint(encoded_data, first=first_ch[0], second=first_ch[1])
+                negative_dp = create_datapoint(encoded_data, first=first_ch[1], second = get_first_half(chapters[cnum+1]))
+                positive_dp["positive"] = True
+                negative_dp["positive"] = False
+                data.add(positive_dp)
+                data.add(negative_dp)
+
+        random.shuffle(data)
+
+        # Write resulting data
+        for d in data:
+            writer.write(d)
--- scripts/shuffle_data.py
+++ scripts/shuffle_data.py
@@ -3,6 +3,12 @@ from transformers import BertModel, BertConfig
 import jsonlines
 import random
 
+def float_type(arg):
+    try:
+        return float(arg)
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"{arg} is not a valid float value")
+
 
 # I'll rethink this file in the future.
 
@@ -10,19 +16,28 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", dest="input", help="File containing gutenberg & women writers project")
-    parser.add_argument("--output", dest="output", help="Output files")
-    parser.add_argument("--train_files", type=int, dest="num_files")
+    parser.add_argument("--output", dest="output", nargs=2, help="Output files")
+    parser.add_argument("--data_size", type=int, dest="data_size")
+    parser.add_argument("--split_ratio", type=float_type, dest="ratio")
     args, rest = parser.parse_known_args()
 
     data = []
-    with jsonlines.open(args.input, mode="r") as reader, jsonlines.open(args.output, mode="w") as writer:
+    with jsonlines.open(args.input, mode="r") as reader, jsonlines.open(args.output[0], mode="w") as train_writer, jsonlines.open(args.output[1], mode="w") as test_writer:
         # For line in jsonlines
         for text in reader:
             data.append(text)
         random.shuffle(data)
 
-        minimum = min(args.num_files, len(data))
-        for i in range(minimum):
-            writer.write(data[i])
+        data_size = min(args.data_size, len(data))
+        tr_size = int(0.8 * len(data))
+        ev_size = int(0.2 * len(data))
+        print(f"Training size: {tr_size}")
+        print(f"Eval size: {ev_size}")
+
+        for i in range(data_size):
+            if i < tr_size:
+                train_writer.write(data[i])
+            else:
+                test_writer.write(data[i])
         
             
